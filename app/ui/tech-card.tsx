a4c76530a6cb1ffde29f284bb5d5ffb9fea89fec
jimmy <jimmysolvent@gmail.com>
2024-09-09 18:16:36 +0100

    fixed. appear-on-scroll now working.

--- app/ui/tech-card.tsx
+++ app/ui/tech-card.tsx
@@ -3,22 +3,83 @@
 import {DotLottieReact} from "@lottiefiles/dotlottie-react";
 import { BiRightArrowAlt } from "react-icons/bi";
 import {useEffect} from "react";
-export default function TechCard({title, body, animation, borderColor}: { title: string, body: string, animation: string, borderColor: string }) {
+export default function TechCard({title, body, animation, borderColor}: {
+    title: string,
+    body: string,
+    animation: string,
+    borderColor: string
+}) {
 
-    useEffect(() => {
-        window.addEventListener(
-            "load",
-            (event) => {
-                thisElement = document.getElementById(title)!;
-
-                createObserver();
-            },
-            false,
-        );
-    })
-
+    console.log('card is running...')
     let thisElement: Element;
 
+    useEffect(() => {
+        console.log(`use effect is running... window is ${window}`)
+
+        function setElementAndCreateObservers() {
+            console.log('window load listener called')
+            thisElement = document.getElementById(title)!;
+            createObserver();
+        }
+
+        if(document.readyState === "complete"){
+            console.log("document is already ready")
+            setElementAndCreateObservers();
+        } else {
+            window.addEventListener(
+                "load",
+                (event) => {
+                    setElementAndCreateObservers();
+                },
+                false,
+            );
+        }
+    }, [])
+
+    //
+    // while(typeof window === "undefined"){
+    //     delay(300);
+    //     setTimeout(function() {
+    //
+    //     }, 500);
+    // }
+
+    // let attempts = 0;
+    //
+    // const delay = (delayInms: number) => {
+    //     return new Promise(resolve => setTimeout(resolve, delayInms));
+    // };
+    //
+    // let thisWindow = window;
+    // async function ensureWindow() {
+    //     while (typeof thisWindow === "undefined") {
+    //         attempts++
+    //         console.log(`this attempt ${attempts}`)
+    //         await delay(500);
+    //         thisWindow = window;
+    //     }
+    // }
+    //
+    // await ensureWindow();
+
+
+
+    // window.addEventListener(
+    //     "load",
+    //     (event) => {
+    //         console.log('window load listener called')
+    //         thisElement = document.getElementById(title)!;
+    //
+    //         createObserver();
+    //     },
+    //     false,
+    // );
+
+    // useEffect(() => {
+    //     const onLoad = () => {
+    //
+    //     }
+    // })
     function createObserver() {
         let observer;
 
@@ -34,7 +95,7 @@ export default function TechCard({title, body, animation, borderColor}: { title:
 
     function handleIntersect(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
         entries.forEach((entry) => {
-            if(entry.isIntersecting) {
+            if (entry.isIntersecting) {
                 let className = entry.target.className;
                 // entry.target.className = className + " techCardAnim";
                 // (entry.target as HTMLElement).style.top = "0";
@@ -47,7 +108,7 @@ export default function TechCard({title, body, animation, borderColor}: { title:
     return <div
         id={title}
         className={
-        `
+            `
         flex
         flex-col
         p-4
